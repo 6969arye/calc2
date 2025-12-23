4b3bacd5be004cad7d263cc36f5211049b8bf4cb
ronb8 <ronbuaron8@gmail.com>
2025-12-23 15:58:23 +0200

    fix errors in test_add in test_calc2.py

--- test_calc2.py
+++ test_calc2.py
@@ -3,8 +3,8 @@ from calc2 import *
 
 def test_add():
     assert add(2, 3) == 5
-    assert add(-1, 1) == 1
-    assert add(0, 0) == 1
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
 
 
 def test_subtract():
